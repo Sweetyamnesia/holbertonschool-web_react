bb94c13290ae05a4c6cd09499db946ab152ab3eb
Sweetyamnesia <angela.r@live.fr>
2025-09-10 16:56:09 +0200

    modification

---
+++ task_2/js/main.ts
@@ -0,0 +1,20 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  location: string;
+  fullTimeEmployee: boolean;
+}
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Director = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
