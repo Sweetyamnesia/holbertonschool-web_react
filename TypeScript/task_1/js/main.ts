04f17d186208a066444f983134d7514d9902539a
Sweetyamnesia <angela.r@live.fr>
2025-09-11 10:15:13 +0200

    implement class with constructor and interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -24,3 +24,26 @@ interface printTeacherFunction {
 function printTeacher(firstName: string, lastName: string) {
 	return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+	firstName: string;
+  	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+   workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
