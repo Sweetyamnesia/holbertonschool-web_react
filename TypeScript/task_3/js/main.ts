3cdf4d811d4057a77740ecef2990bd5bcd4197c2
Sweetyamnesia <angela.r@live.fr>
2025-09-12 11:20:52 +0200

    delete lastname inside update row, change for age

--- TypeScript/task_3/js/main.ts
+++ TypeScript/task_3/js/main.ts
@@ -10,7 +10,7 @@ let row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = { ...row, lastName: "Smith" };
+const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 
 CRUD.deleteRow(newRowID);
