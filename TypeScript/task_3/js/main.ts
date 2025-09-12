0a566ecddda5bf22d49201faecaa2a723a667265
Sweetyamnesia <angela.r@live.fr>
2025-09-12 11:18:55 +0200

    fix: change triple slash directive and import CRUD

--- TypeScript/task_3/js/main.ts
+++ TypeScript/task_3/js/main.ts
@@ -1,11 +1,14 @@
-/// import { rowID, rowElement } from './interface.ts'; import * as CRUD from './crud.js';
+/// <reference path="./crud.d.ts" />
+/// <reference path="./interface.ts" />
+
+import * as CRUD from './crud.js';
 
 let row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva",
 };
 
-const newRowID: RowID = insertRow(row);
+const newRowID: RowID = CRUD.insertRow(row);
 
 const updatedRow: RowElement = { ...row, lastName: "Smith" };
 CRUD.updateRow(newRowID, updatedRow);
